074e2ee044bea409ba554d4a76c81d0a574893da
Henrique-C-C <cabrl.henrique@gmail.com>
2026-05-01 23:18:06 -0300

    Quase acabando

--- programa.py
+++ programa.py
@@ -1,24 +1,45 @@
 import random
 import funcoes
 
-rerrolagens = 0
-lista_dados_guardados = []
-lista_faces_dados = []
 opções = [1,2,3,4,0]
 
-i = 0
-
-while i < 5:
-
-    face = random.randint(1,6) 
-
-    lista_faces_dados.append(face)
-
-    i +=1
+cartela = {
+    'regra_simples':  {
+        1:-1,
+        2:-1,
+        3:-1,
+        4:-1,
+        5:-1,
+        6:-1
+    },
+    'regra_avancada' : {
+        'sem_combinacao':-1,
+        'quadra': -1,
+        'full_house': -1,
+        'sequencia_baixa': -1,
+        'sequencia_alta': -1,
+        'cinco_iguais': -1
+    }
+}
 
 j = 0
 
 while j < 12:
+
+    rerrolagens = 0
+    lista_dados_guardados = []
+    lista_faces_dados = []
+    listacombinação = []
+
+    i = 0
+
+    while i < 5:
+
+        face = random.randint(1,6) 
+
+        lista_faces_dados.append(face)
+
+        i +=1
 
     print(f"Dados rolados: {lista_faces_dados}")
     print(f"Dados guardados: {lista_dados_guardados}")
@@ -28,51 +49,75 @@ while j < 12:
     while opção not in opções:
 
         print("Opção inválida. Tente novamente.")
+        opção = int(input(""))
+    
+    while opção != 0: 
+        if opção == 1:
+            
+            print("Digite o índice do dado a ser guardado (0 a 4):")
+            índice = int(input(""))
+            lista_dados_guardados.append(lista_faces_dados[índice])
+            del lista_faces_dados[índice]
+
+        elif opção == 2:
+
+            print("Digite o índice do dado a ser removido (0 a 4):")
+            dado_para_remover = int(input(""))
+            lista_faces_dados.append(lista_dados_guardados[dado_para_remover])
+            del lista_dados_guardados[dado_para_remover]
+
+        elif opção == 3 and rerrolagens < 2:
+            
+            rerrolagens += 1
+            tamanho = len(lista_faces_dados)
+            k = 0
+            lista_faces_dados = []
+
+            while k < tamanho:
+
+                face = random.randint(1,6) 
+
+                lista_faces_dados.append(face)
+
+                k +=1
+
+        elif opção == 3 and rerrolagens == 2:
+            
+            print("Você já usou todas as rerrolagens.")
+
+        elif opção == 4:
+
+            funcoes.imprime_cartela(cartela)
+
         print(f"Dados rolados: {lista_faces_dados}")
         print(f"Dados guardados: {lista_dados_guardados}")
         print("Digite 1 para guardar um dado, 2 para remover um dado, 3 para rerrolar, 4 para ver a cartela ou 0 para marcar a pontuação:")
         opção = int(input(""))
 
-    if opção == 1:
+    if opção == 0:
+
+        print("Digite a combinação desejada:")
+        combinação = input("")
         
-        print("Digite o índice do dado a ser guardado (0 a 4): ")
-        índice = int(input(""))
-        lista_dados_guardados.append(lista_faces_dados[índice])
-        del lista_faces_dados[índice]
+        if combinação  in listacombinação:
+            print("Essa combinação já foi utilizada.")
 
-    if opção == 2:
+        if combinação not in cartela:
+            print("Combinação inválida. Tente novamente.")
 
-        print("Digite o índice do dado a ser removido (0 a 4): ")
-        dado_para_remover = int(input(""))
-        lista_faces_dados.append(lista_dados_guardados[dado_para_remover])
-        del lista_dados_guardados[dado_para_remover]
-
-    if opção == 3 and rerrolagens < 2:
-        
-        rerrolagens += 1
-        tamanho = len(lista_faces_dados)
-        k = 0
-        lista_faces_dados = []
-
-        while k < tamanho:
-
-            face = random.randint(1,6) 
-
-            lista_faces_dados.append(face)
-
-            k +=1
-
-    elif opção == 3 and rerrolagens == 2:
-        
-        print("Você já usou todas as rerrolagens.")
-
-    if opção == 4:
-
-        print(funcoes.imprime_cartela)
-
-    #if opção == 0:
+        listacombinação.append(combinação)
 
 
-    j +=1
+        funcoes.faz_jogada(lista_faces_dados,combinação,cartela)
 
-        
+        j +=1
+
+pontuação = 0
+
+for regras,dicionario in cartela.items():
+
+    for combinações,valores in dicionario.items():
+
+        pontuação += valores
+
+print("Pontuação total: {pontuacao}")
